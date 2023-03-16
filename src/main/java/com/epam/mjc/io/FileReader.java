f3b97f94b188b9659065bd8955de153351815bdc
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-03-16 22:45:29 +0300

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,29 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.*;
+import java.util.Scanner;
 
 public class FileReader {
-
-    public Profile getDataFromFile(File file) {
-        return new Profile();
-    }
+ public Profile getDataFromFile(File file) throws IOException{
+  Scanner s=null;
+  try{
+   s=new Scanner(file);
+   s.useDelimiter("\n?\\w+: ");
+   String name1=s.next();
+   int age1=s.nextInt();
+   String email1=s.next();
+   long phone1=s.nextLong();   
+   Profile p=new Profile(name1,age1,email1,phone1);
+   return p;    
+  }finally{
+   if(s!=null){
+    s.close();}
+  }
+ } 
+ public static void main(String[] args) throws IOException{
+  FileReader fileReader=new FileReader();
+  File f=new File("Profile.txt");
+  fileReader.getDataFromFile(f);
+ }
 }
